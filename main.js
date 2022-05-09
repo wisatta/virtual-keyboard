ccdf8d049df64264af7f7a7a504cfe891ffda64b
Anastasiya <90135440+wisatta@users.noreply.github.com>
2022-05-09 18:33:51 +0300

    fix: clean the code

--- main.js
+++ main.js
@@ -18,15 +18,6 @@ input.className = 'input-text';
 document.body.append(input);
 
 const keyboard = [49, 50, 51, 52, 53, 54, 55, 56, 57, 48, 45, 61, 113, 119, 101, 114, 116, 121, 117, 105, 111, 112, 91, 93, 13, 97, 115, 100, 102, 103, 104, 106, 107, 108, 59, 39, 92, 122, 120, 99, 118, 98, 110, 109, 44, 46, 47];
-// const keyboard = [113, 119, 101, 114, 116, 121, 117, 105, 111, 112, 91, 93, 97, 115, 100, 102, 103, 104, 106, 107, 108, 59, 39, 92, 122, 120, 99, 118, 98, 110, 109, 44, 46, 47];
-
-// Get keyboard
-
-// document.onkeypress = function(event){
-//     // console.log(event);
-//     keyboard.push(event.charCode);
-//     console.log(keyboard);
-// }
 
 // Create keyboard
 
@@ -58,9 +49,6 @@ function removeAnimation() {
 }
 
 document.onkeypress = function(event) {
-    // console.log(event.code); // KeyX
-    // console.log(event.keyCode); // 120
-    // console.log(event.key) // x
     document.querySelector('#keyboard-frame .key[data="'+event.keyCode+'"]').classList.add('active');
     document.querySelector('#keyboard-frame .key[data="'+event.keyCode+'"]').classList.add('active-animation');
     setTimeout(removeActiveClass, 300);
@@ -76,8 +64,6 @@ document.querySelectorAll('#keyboard-frame .key').forEach(function(el){
         this.classList.add('active-animation');
         setTimeout(removeActiveClass, 300);
         setTimeout(removeAnimation, 300);
-        // console.log(code);
-        // console.log(String.fromCharCode(code));
         let chr = String.fromCharCode(code);
         input.value = chr;
     }
